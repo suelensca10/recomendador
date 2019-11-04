5417f3e226bd415ad3ff06d73217e0a7295b74f6
suelen <sscariott@ucs.br>
2019-11-04 00:15:19 -0300

    engine de recomendacao com problemas pra resolver

--- app/models.py
+++ app/models.py
@@ -1,10 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.core.files import File
-import os
-from urllib.request import urlopen
-from django.core.files.temp import NamedTemporaryFile
-#import requests
+from django_pandas.managers import DataFrameManager
 
 class Book(models.Model):
     book = models.IntegerField()
@@ -14,16 +10,9 @@ class Book(models.Model):
     average_rating = models.FloatField()
     ratings_count = models.FloatField()
     image_url = models.URLField()
-    #image_file = models.ImageField(upload_to='images/', null = True, blank=True)
 
-   # def save_image_from_url(model):
-   #     r = requests.get(model.image_url)
-   #     img_temp = NamedTemporaryFile(delete=True)
-   #     img_temp.write(r.content)
-   #     img_temp.flush()
-   #     model.image_file.save("image.jpg", File(img_temp), save=True)
+    objects = DataFrameManager()
 
-    #se for pra usar, é esse aqui
     #def get_remote_image(self):
     #    if self.image_url and not self.image_file:
     #        result = urllib.urlretrieve(self.image_url)
@@ -33,14 +22,6 @@ class Book(models.Model):
     #        )
     #        self.save()
 
-    #def get_remote_image(self):
-    #    if self.image_url and not self.image_file:
-    #        img_temp = NamedTemporaryFile(delete=True)
-    #        img_temp.write(urlopen(self.image_url).read())
-    #        img_temp.flush()
-    #        self.image_file.save(f"image_{self.pk}", File(img_temp))
-    #    self.save()"
-
     def __str__(self):
         return self.title
 
@@ -48,3 +29,13 @@ class Rating(models.Model):
     user_id = models.IntegerField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     rating = models.IntegerField()
+
+    objects = DataFrameManager()
+
+class PivotData(models.Model):
+    row_col_a = models.CharField(max_length=15)
+    row_col_b = models.CharField(max_length=15)
+    row_col_c = models.CharField(max_length=15)
+    value_col_d = models.FloatField()
+    value_col_e = models.FloatField()
+    value_col_f = models.FloatField()
--- app/views.py
+++ app/views.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views import generic
 from sklearn.cluster import KMeans
 import pandas as pd
-import numpy as np
 import helper
-import sqlite3
+from django_pivot.pivot import pivot
+from django_pivot.histogram import histogram
 
 from .forms import RatingForm
 from .models import Book, Rating
@@ -39,16 +37,70 @@ def rating_new(request):
     form = RatingForm()
     return render(request, 'app/rating_edit.html', {'form': form})
 
-#def get_recommendations(user_id)
-    #ler tabelas do banco de dados
-    #Books = Book.objects
-    #Ratings = Ratings.objects
-    #ratings_title = pd.merge(Ratings, Books[['book_id', 'title']], on='book_id')
- #   return list_of_recommended_books
-
 @login_required
 def result(request):
-    Books = Book.objects
-    Ratings = Rating.objects
-    return render(request, 'app/result.html')
+
+    #cria querysets de todos os registros das tabelas book e rating
+    qs_Books = Book.objects.all()
+    #qs_Ratings = Rating.objects.all()
+    qs_Ratings = Rating.objects.order_by('-user_id')[:200]
+
+    #cria pandas dataframes baseados nos querysets
+    Books = qs_Books.to_dataframe()
+    Ratings = qs_Ratings.to_dataframe()
+
+    #transforma os dataframes para formato desejado (matrix uxers X books)
+    #?? trocar o título do livro pelo id antes do merge???
+    ratings_title = pd.merge(Ratings, Books[['book', 'title']], on='book')
+    #user_book_ratings = pd.pivot_table(ratings_title, index='user_id', columns='title', values='rating')
+    user_book_ratings = pivot(ratings_title, index='user_id', columns='title', values='rating')
+
+    # antes de resumir o dataframe guarda o row ref ao usuário logado
+    curr_user = request.user
+    curr_user_id = curr_user.id
+    usu_atual = user_book_ratings.loc[curr_user_id]
+
+    # resume o dataframe por questões de performance
+    n_books = 1001
+    n_users = 1000
+    most_rated_books = helper.sort_by_rating_density(user_book_ratings, n_books, n_users)
+
+    # coloca de volta o row referente ao usuário logado
+    most_rated_books = most_rated_books.append(usu_atual)
+
+    # substitui nulls por zero
+    most_rated_books = most_rated_books.fillna(0)
+
+    kmeans = KMeans(n_clusters=4, init='k-means++')
+    kmeans.fit(most_rated_books);
+
+    # prediz em qual cluster cada usuário está inserido
+    predictions = kmeans.predict(most_rated_books)
+
+    # Cria uma coluna com o cluster correspondente a cada usuário
+    most_rated_books['cluster'] = kmeans.labels_
+
+    # descobre o número do cluster do usuário atual e filtra o dataframe para manter apenas quem é do mesmo cluster q ele
+    cluster_number = most_rated_books.loc[curr_user_id, 'cluster']
+    cluster = most_rated_books[most_rated_books.cluster == cluster_number].drop(['cluster'], axis=1)
+
+    # Retorna todas as ratings do usuário atual
+    user_ratings = pd.DataFrame(most_rated_books.loc[curr_user_id])
+    user_ratings.columns = ['rating']
+    user_ratings.shape
+
+    # Quais livros o usuário não leu (não pontou)?
+    user_unrated_books = user_ratings[user_ratings['rating'] == 0]
+
+    # Média das ratings dos membros desse cluster para os livros que o usuário ainda não leu
+    avg_ratings = pd.DataFrame(pd.concat([user_unrated_books, cluster.mean()], axis=1, join='inner').loc[:, 0])
+    avg_ratings.columns = ['mean_rate']
+
+    # Ordena pela pontuação de maneira decrescente (as maiores ratings ficam por cima)
+    avg_ratings = avg_ratings.sort_values(by='mean_rate', ascending=False)[:10]
+
+    # retorna só os títulos dos livros - cria um novo dataframe com od indexes
+    recomendations = pd.DataFrame(avg_ratings.index)
+
+    return render(request, 'app/result.html', {'recomendations': recomendations})
 
