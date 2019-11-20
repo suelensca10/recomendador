3f71b3284381c98acd3ac991bd48222a02019a8d
suelen <sscariott@ucs.br>
2019-11-20 00:23:24 -0300

    mais calibradas nas recomendações

--- app/views.py
+++ app/views.py
@@ -40,36 +40,6 @@ def rating_new(request):
 
 @login_required
 def result(request):
-
-    #Código que estava no helper.py
-    def get_most_rated_movies(user_movie_ratings, max_number_of_movies):
-        # 1- Count
-        user_movie_ratings = user_movie_ratings.append(user_movie_ratings.count(), ignore_index=True)
-        # 2- sort
-        user_movie_ratings_sorted = user_movie_ratings.sort_values(len(user_movie_ratings) - 1, axis=1, ascending=False)
-        user_movie_ratings_sorted = user_movie_ratings_sorted.drop(user_movie_ratings_sorted.tail(1).index)
-        # 3- slice
-        most_rated_movies = user_movie_ratings_sorted.iloc[:, :max_number_of_movies]
-        return most_rated_movies
-
-    # Código que estava no helper.py
-    def get_users_who_rate_the_most(most_rated_movies, max_number_of_movies):
-        # Get most voting users
-        # 1- Count
-        most_rated_movies['counts'] = pd.Series(most_rated_movies.count(axis=1))
-        # 2- Sort
-        most_rated_movies_users = most_rated_movies.sort_values('counts', ascending=False)
-        # 3- Slice
-        most_rated_movies_users_selection = most_rated_movies_users.iloc[:max_number_of_movies, :]
-        most_rated_movies_users_selection = most_rated_movies_users_selection.drop(['counts'], axis=1)
-        return most_rated_movies_users_selection
-
-    # Código que estava no helper.py
-    def sort_by_rating_density(user_movie_ratings, n_movies, n_users):
-        most_rated_movies = get_most_rated_movies(user_movie_ratings, n_movies)
-        most_rated_movies = get_users_who_rate_the_most(most_rated_movies, n_users)
-        return most_rated_movies
-
     #dados do usuário logado
     curr_user = request.user
     curr_user_id = curr_user.id
@@ -96,39 +66,20 @@ def result(request):
     ratings_title = pd.merge(Ratings, Books[['book', 'title']], on='title')
     user_book_ratings = pd.pivot_table(ratings_title, index='user_id', columns='title', values='rating') #pandas puro
 
-    # antes de resumir o dataframe guarda os rows ref ao usuário logado
-    usu_atual = pd.DataFrame(user_book_ratings.loc[curr_user_id]).T
-
-    # resume o dataframe por questões de performance
-    #n_books = 1001
-    #n_users = 1000
-    #most_rated_books = sort_by_rating_density(user_book_ratings, n_books, n_users)
-    most_rated_books = user_book_ratings
-
-    # coloca de volta os rows referente ao usuário logado
-    #most_rated_books = pd.merge(most_rated_books, usu_atual, how='outer')  # Comum em apenas uma delas
-
     # substitui nulls por zero
-    most_rated_books = most_rated_books.fillna(0)
-
-    #retira os livros chatos do dataframe
-   # most_rated_books.drop([''], axis=1)
-
+    user_book_ratings = user_book_ratings.fillna(0)
     kmeans = KMeans(n_clusters=6, init='k-means++')
-    kmeans.fit(most_rated_books);
-
-    # prediz em qual cluster cada usuário está inserido
-    predictions = kmeans.predict(most_rated_books)
+    kmeans.fit(user_book_ratings);
 
     # Cria uma coluna com o cluster correspondente a cada usuário
-    most_rated_books['cluster'] = kmeans.labels_
+    user_book_ratings['cluster'] = kmeans.labels_
 
     # descobre o número do cluster do usuário atual e filtra o dataframe para manter apenas quem é do mesmo cluster q ele
-    cluster_number = most_rated_books.loc[curr_user_id, 'cluster']
-    cluster = most_rated_books[most_rated_books.cluster == cluster_number].drop(['cluster'], axis=1) #acho q é aqui o problema
+    cluster_number = user_book_ratings.loc[curr_user_id, 'cluster']
+    cluster = user_book_ratings[user_book_ratings.cluster == cluster_number].drop(['cluster'], axis=1)
 
     # Retorna todas as ratings do usuário atual
-    user_ratings = pd.DataFrame(most_rated_books.loc[curr_user_id])
+    user_ratings = pd.DataFrame(user_book_ratings.loc[curr_user_id])
     user_ratings.columns = ['rating']
 
     # Quais livros o usuário não leu (não pontou)?
@@ -141,16 +92,15 @@ def result(request):
     # Ordena pela pontuação de maneira decrescente (as maiores ratings ficam por cima)
     avg_ratings = avg_ratings.sort_values(by='mean_rate', ascending=False)[:12]
 
-    # retorna só os títulos dos livros - cria um novo dataframe com od indexes
+    # retorna só os títulos dos livros - cria um novo dataframe com os indexes
     recomendations = avg_ratings.index
-
     recomendations = recomendations.values.tolist()
-
     books_result = []
 
+    #vai incrementando a lista de resultados (book_result)1
     for title in recomendations:
         books_result.append(Book.objects.filter(title=title).first())
 
-    #return render(request, 'app/result.html', {'recomendations': recomendations})
+    #retorna resultado para a template de result
     return render(request, 'app/result.html', {'recomendations': books_result})
 
